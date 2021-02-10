2466ba453beaec4031760e458025172727cb486a
Stu Pender <stu1992@gmail.com>
2021-02-09 18:54:55 -0600

    javascript-working

--- script.js
+++ script.js
@@ -1,31 +1,134 @@
-function add(...numbers) {
-	let value = 0;
-	for (var i = 0; i < numbers.length; i++) {
-		value += numbers[i];
-	}
-	return value;
+// VARRIABLES TO HOLD SCREEN VALUE
+let numberButtons = document.querySelectorAll('.number');
+let pointButton = document.querySelector('.decimal');
+let operatorButtons = document.querySelectorAll('.operand');
+let equalsButton = document.querySelector('.equal');
+let clearButton = document.querySelector('button.clear');
+let screen = document.querySelector('#screen');
+let deleteButton = document.querySelector('.backspace');
+
+
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let shouldResetScreen = false;
+
+
+window.addEventListener("keydown", setInput);
+equalsButton.addEventListener("click", evaluate);
+clearButton.addEventListener("click", clear);
+deleteButton.addEventListener("click", deleteNumber);
+pointButton.addEventListener("click", appendPoint);
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener("click", () => setOperation(button.textContent))
+);
+
+function appendNumber(number) {
+  if (screen.textContent === "0" || shouldResetScreen) resetScreen();
+  screen.textContent += number;
 }
 
-function subtract(...numbers) {
-	let value = numbers[0];
-	for (var i = 1; i < numbers.length; i++) {
-		value -= numbers[i];
-	}
-	return value;
+function resetScreen() {
+  screen.textContent = "";
+  shouldResetScreen = false;
 }
 
-function multiply(...numbers) {
-	let value = numbers[0];
-	for (var i = 1; i < numbers.length; i++) {
-		value *= numbers[i];
-	}
-	return value;
+function clear() {
+  screen.textContent = "0";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
 }
 
-function divide(...numbers) {
-	let value = numbers[0];
-	for (var i = 1; i < numbers.length; i++) {
-		value /= numbers[i];
-	}
-	return value;
+function appendPoint() {
+  if (shouldResetScreen) resetScreen();
+  if (screen.textContent === "") screen.textContent = "0";
+  if (screen.textContent.includes(".")) return;
+  screen.textContent += ".";
+}
+
+function deleteNumber() {
+    screen.textContent = screen.textContent.toString().slice(0, -1);
+}
+
+// OPERATION FUNCTIONS
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate();
+  firstOperand = screen.textContent;
+  currentOperation = operator;
+  shouldResetScreen = true;
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return;
+  if (currentOperation === "÷" && screen.textContent === "0") {
+    alert("A number cannot be divided by 0");
+    clear();
+    return;
+  }
+  secondOperand = screen.textContent;
+  screen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  );
+  currentOperation = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 10) / 10;
+}
+
+function setInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === ".") appendPoint();
+  if (e.key === "=" || e.key === "Enter") evaluate();
+  if (e.key === "Backspace") deleteNumber();
+  if (e.key === "Escape") clear();
+  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+    setOperation(convertOperator(e.key));
+}
+
+function convertOperator(keyboardOperator) {
+  if (keyboardOperator === "/") return "÷";
+  if (keyboardOperator === "*") return "*";
+  if (keyboardOperator === "-") return "−";
+  if (keyboardOperator === "+") return "+";
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
 }
